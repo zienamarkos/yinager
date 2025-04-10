d31207d822b88809769e07de28eaa3dd971d8164
zienamarkos <zienamarkos78@gmail.com>
2025-04-10 17:28:47 +0300

    for deployment

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.mjs
-export default {
-    reactStrictMode: true,
-    swcMinify: true, // If you're using SWC for minification (recommended for better performance)
-    webpack(config) {
-      return config;
-    },
-  };
-  
